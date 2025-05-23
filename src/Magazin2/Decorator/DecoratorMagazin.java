81d1e357ee978fece2c0222e22e0b9b44994bcdb
Ana-Maria Pamfil <ana.pamfil@hotmail.com>
2025-05-24 02:42:39 +0300

    commit

--- src/Magazin2/Decorator/DecoratorMagazin.java
+++ src/Magazin2/Decorator/DecoratorMagazin.java
@@ -1,4 +1,16 @@
 package Magazin2.Decorator;
 
-public class DecoratorMagazin {
+import Magazin2.Interfaces.*;
+
+public abstract class DecoratorMagazin implements IMagazin{
+    protected IMagazin magazin;
+
+   public DecoratorMagazin(IMagazin magazin){ this.magazin=magazin;}
+
+    @Override
+    public double getTotal(ICosCumparaturi cos, IClient client) {
+        return magazin.getTotal(cos, client);
+    }
+    public abstract void aplicaDiscount(ICosCumparaturi cos, IClient client);
+
 }
