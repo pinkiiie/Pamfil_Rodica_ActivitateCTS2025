718d9b7ae87460b472ef6f086257d3ba7e3e96c2
Ana-Maria Pamfil <ana.pamfil@hotmail.com>
2025-05-24 02:42:56 +0300

    commit

--- src/Magazin2/Clase/Produs.java
+++ src/Magazin2/Clase/Produs.java
@@ -1,4 +1,40 @@
 package Magazin2.Clase;
 
-public class Produs {
+import Magazin2.Interfaces.IProdus;
+
+public class Produs implements IProdus {
+    private float pret;
+    private int cantitate;
+    private String denumire;
+
+    public Produs(float pret, int cantitate, String denumire) {
+        this.pret = pret;
+        this.cantitate = cantitate;
+        this.denumire = denumire;
+    }
+
+    public float getPret() {
+        return this.pret;
+    }
+
+    @Override
+    public int getCantitate() {
+        return this.cantitate;
+    }
+
+    @Override
+    public String getDenumire() {
+        return this.denumire;
+    }
+
+    @Override
+    public String toString() {
+        return "Produs{" +
+                "pret=" + pret +
+                ", cantitate=" + cantitate +
+                ", denumire='" + denumire + '\'' +
+                '}';
+
+
+    }
 }
